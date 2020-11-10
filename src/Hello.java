73af8098dd30f6e420da1a456aaadfa7ec6d28ec
Dean Wynn <wynnkhalifa117@gmail.com>
2020-11-10 11:26:29 -0600

    Added a new text Hello file

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello there");
+        // Mac users and windows users are happy to see commit dialog
     }
 }
